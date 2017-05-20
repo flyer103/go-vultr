62867813879b692bc1311f3be9fb5785b1311445
flyer <flyer103@gmail.com>
2017-05-20 22:31:48 +0800

    execute API request parallelly

--- pkg/vultr/common.go
+++ pkg/vultr/common.go
@@ -15,6 +15,13 @@ const (
 	APINameServerReboot
 )
 
+type APITask struct {
+	SubID  string
+	MainIP string
+	Label  string
+	Error  error
+}
+
 func (vc *Client) PostSubID(apiName APIName, subID string) error {
 	var api string
 	switch apiName {
@@ -55,27 +62,45 @@ func (vc *Client) PostAllSubIDs(apiName APIName) ([]ServerBatchResult, error) {
 		return nil, err
 	}
 
+	ch := make(chan ServerBatchResult, len(serverInfo))
 	results := make([]ServerBatchResult, 0, len(serverInfo))
 	for subID, info := range serverInfo {
-		var err error
-		switch apiName {
-		case APINameServerReinstall:
-			err = vc.ServerReinstall(subID)
-		case APINameServerHalt:
-			err = vc.ServerHalt(subID)
-		case APINameServerStart:
-			err = vc.ServerStart(subID)
-		case APINameServerReboot:
-			err = vc.ServerReboot(subID)
-		}
-		res := ServerBatchResult{
+		task := APITask{
 			SubID:  subID,
 			MainIP: info.MainIP,
 			Label:  info.Label,
-			Error:  err,
 		}
+
+		go vc.doAPITask(apiName, task, ch)
+	}
+
+	for i := 0; i < len(serverInfo); i++ {
+		res := <-ch
 		results = append(results, res)
 	}
 
 	return results, nil
+}
+
+func (vc *Client) doAPITask(apiName APIName, task APITask, ch chan ServerBatchResult) {
+	var err error
+	switch apiName {
+	case APINameServerReinstall:
+		err = vc.ServerReinstall(task.SubID)
+	case APINameServerHalt:
+		err = vc.ServerHalt(task.SubID)
+	case APINameServerStart:
+		err = vc.ServerStart(task.SubID)
+	case APINameServerReboot:
+		err = vc.ServerReboot(task.SubID)
+	}
+
+	res := ServerBatchResult{
+		SubID:  task.SubID,
+		MainIP: task.MainIP,
+		Label:  task.Label,
+		Error:  err,
+	}
+
+	ch <- res
 }
